40d7be2202dc6dcbd5f283d2171d3b0589f28a03
Shimao Zhang <v-shimzhang@microsoft.com>
2025-04-07 09:18:56 +0000

    Commit for updating pretrained model test hyperparameters

--- evaluate.sh
+++ evaluate.sh
@@ -24,12 +24,12 @@ INFERENCE_SPECIFIC_ARGS=(
     --attention-dropout 0.0
     --hidden-dropout 0.0
     --micro-batch-size 3
-    --results-path /mnt/shimao-zhang-blob/multi-mix/evaluation_results/zhtest_llama_3B_en100_0215.json
+    --results-path /mnt/shimao-zhang-blob/multi-mix/evaluation_results/mmlu_llama_3B_en100_0215.json
     # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
     # --task-list gsm8k,mmlu_pro_math
     # --task-list math_continuation
-    # --task-list mmlu_continuation,mmlu
-    --task-list cmmlu
+    --task-list mmlu_continuation,mmlu
+    # --task-list cmmlu
     --num-fewshot 5
     --trust-remote-code
 )
