3e735c8aca50226c1f53a1308394dc2a753bd2db
Shimao Zhang <v-shimzhang@microsoft.com>
2025-04-24 07:16:37 +0000

    Commit for convert model

--- convert_model.sh
+++ convert_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-LOAD_DIR='/mnt/shimao-zhang-blob/multi-mix/pretraining/llama_3B_multilingual_mix_enzh150_0419'
-TARGET_DIR='/mnt/shimao-zhang-blob/multi-mix/pretraining/llama_3B_multilingual_mix_enzh150_0419/merged'
+LOAD_DIR='/mnt/shimao-zhang-blob/multi-mix/pretraining/llama_3B_zh100_0422'
+TARGET_DIR='/mnt/shimao-zhang-blob/multi-mix/pretraining/llama_3B_zh100_0422/merged'
 # LOAD_DIR='/mnt/blob-pretraining-hptrainingwestcentralus/checkpoints/OpenPAI-Pretrain-17BA3B-RoPE-HQ-0405'
 # TARGET_DIR='/mnt/blob-pretraining-hptrainingwestcentralus/checkpoints/OpenPAI-Pretrain-17BA3B-RoPE-HQ-0405/iter_196000_merged'
 
