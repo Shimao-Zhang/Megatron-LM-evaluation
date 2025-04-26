cfe4362679c7d27e57ac7d90b0fd6025ce0f396d
Shimao Zhang <v-shimzhang@microsoft.com>
2025-04-26 14:44:07 +0000

    Commit for enzh mix eval

--- evaluate.sh
+++ evaluate.sh
@@ -27,16 +27,16 @@ INFERENCE_SPECIFIC_ARGS=(
     --attention-dropout 0.0
     --hidden-dropout 0.0
     --micro-batch-size 3
-    --results-path /mnt/shimao-zhang-blob/multi-mix/evaluation_results/llama_3B_en100_0405.json
+    --results-path /mnt/shimao-zhang-blob/multi-mix/evaluation_results/mmlu_llama_3B_en100_0405.json
     # --results-path /mnt/shimao-zhang-blob/multi-mix/evaluation_results/OpenPAI-Pretrain-17BA3B-RoPE-HQ-0405/iter_196000/ceval.json
-    --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
+    # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
     # --task-list gsm8k,mmlu_pro_math
     # --task-list math_continuation
-    # --task-list mmlu_continuation,mmlu
+    --task-list mmlu_continuation,mmlu
     # --task-list cmmlu
     # --task-list ceval-valid
     # --task-list mmlu_continuation,mmlu
-    --num-fewshot 0
+    --num-fewshot 5
     --trust-remote-code
 )
 
